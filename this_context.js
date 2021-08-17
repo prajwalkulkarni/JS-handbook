c57e0e573fda1da39036eb11306f2707fe0b18e8
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-17 10:28:23 +0530

    Update this_context.js

--- this_context.js
+++ this_context.js
@@ -14,3 +14,35 @@ function fn(){
 
 //Context of "this" can be altered based on the object that's calling the function, method invocation(Read below).
 
+
+//2. Method invocation. Function invoked on an objext is method invocation. "this" in method invocation refers to the object that the function is invoked on.
+
+obj = {
+  property: function(){
+    console.log(this==obj) //true
+  }
+}
+
+obj.property() // method invocation
+
+//3. Constructor invocation
+// Constructor is invoked with 'new' keyword, "this" refers to the created object
+// a = B() -> "this" in class/function B refers to execution environment.
+// a = new B() ->"this" in class/function B refers to object 'a'.
+
+class B{
+  constructor(){
+    console.log(this == window)
+  }
+}
+
+obj1 = new B() //false
+obj2 = B()     //Uncaught error, cannot be invoked without new keyword.
+
+function B(){
+  console.log(this==window)
+}
+
+obj1 = new B() //false
+obj1 = B() //true
+
