b890598a199b37693a3eee3a62f85fbbb169987a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-19 14:12:52 +0530

    Update this_context.js

--- this_context.js
+++ this_context.js
@@ -46,3 +46,41 @@ function B(){
 obj1 = new B() //false
 obj1 = B() //true
 
+
+
+//4. Indirect invocation
+
+/* I indirect invocation using methods such as call, bind and apply, we're either changing "this" reference or hooking the "this" to the wrapper function's context.
+*/
+
+obj = {
+  prop:10,
+  
+  fn:function(){
+    console.log(this==obj) //true
+    
+    function innerfunction(){
+      console.log(this==window)//true, if no modifiers are applied, i.e no indirect invocation is made.
+    }
+    return innerfunction.call(this) // changeing "this" context in innerfunction from window to this's context in the function fn, i.e obj.
+  }
+}
+
+obj.fn()
+//OR
+x = obj.fn.apply(x)
+x()
+
+
+//5. Bind functioon invocation
+/*Bound functions context is strict and cannot be manipulated even with the use of indirect invocation.
+i.e, using call or apply.*/
+function fn(){
+  console.log(this==window)
+}
+
+x = fn
+x() //true
+
+x = fn.bind(x)
+x() // changing "this"'s reference from window to the object 'x'.
