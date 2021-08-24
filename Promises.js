44647f0513d29106abda5458f7ab8d5b2324c7db
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-24 11:07:24 +0530

    Create Promises.js

---
+++ Promises.js
@@ -0,0 +1,6 @@
+/* A promise is an object that is sure to return a resolved or failed state. Promises could be used at places where there is uncertaininity of getting the expected results, at such instances
+it is important to handle errors or failures if occured, this could to accomplished using promises*/
+
+//Example snippet
+
+function foo(){
