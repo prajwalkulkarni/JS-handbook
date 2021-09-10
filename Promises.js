476546a6285bf9e2deabf1b1ae8723a313133633
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-10 23:48:19 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -23,5 +23,6 @@ foo(3).then(res=>{
 //Outputs err callback result :- "Odd number"
 
 //In promise chaining subsequent then blocks are executed only if the previous then block returns a resolved promise object.
-//If in any then blocks, if anything(including but not limited to, calling rejection function , throwing error) or anything that'd bring a erranous state is handled by the catch block.
+//If in any then blocks, if anything(including but not limited to, calling rejection function , 
+//throwing error) or anything that'd cause an erranous state is handled by the catch block.
   
