088664940f795e93f0b339ae19662df9c439c00a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-03 23:02:11 +0530

    Update redux_basics.js

--- redux/redux_basics.js
+++ redux/redux_basics.js
@@ -104,7 +104,12 @@ const store = configureStore({
 
 export const sliceActions = slice.actions
 export default store
-//If the reducers are nested in configureStore, state objects can be accessed by drilling thorugh reducer keys of configure store
+//If the reducers are nested in configureStore, state objects can be accessed by drilling through reducer keys of configure store
+//e.g in function component
+export default function Compoenet(){
+  const selector = useSelector(arg=>arg.sliceA.stateAttributeOfSliceA)
+  ...
+}
 
 //Action creators
 /* Action creators can be used to club multiple actions and remove code duplication and redundancy in useEffect(e.g only) or avoid multiple dipatch functions.
