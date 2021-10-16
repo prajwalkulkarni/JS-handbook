6bc4d98a1669132d06fdd9515d3bf267047a993a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-16 23:02:14 +0530

    Update testing-library.js

--- react-test/testing-library.js
+++ react-test/testing-library.js
@@ -1,1 +1,40 @@
 
+import { screen,render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import Greeting from "./Greeting"
+
+describe('Greeting component',()=>{
+    test('Greeting component hello world text',()=>{
+        //Arrange
+        render(<Greeting/>)
+    
+        //Act
+        // ...nothing
+    
+        //Assert
+        let linkElement = screen.getByText(/hello world/i)
+        expect(linkElement).toBeInTheDocument()
+    })
+
+    test('Check if button clicked or not',()=>{
+
+        render(<Greeting/>)
+
+        let linkElement = screen.getByText(/P tag/i)
+        expect(linkElement).toBeInTheDocument()
+    })
+
+    test('Button click state change',()=>{
+        //Arrange
+        render(<Greeting/>)
+
+        //Act
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        //assert
+        const outPutElement = screen.getByText(/changed/i)
+        expect(outPutElement).toBeInTheDocument()
+    })
+}
+)
