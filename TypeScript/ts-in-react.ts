6d31b2ca8c61031602524e489e81c98962bd516b
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-30 20:04:22 +0530

    Update ts-in-react.ts

--- TypeScript/ts-in-react.ts
+++ TypeScript/ts-in-react.ts
@@ -1,1 +1,23 @@
+//Using TypeScript in React is a little more advanced, especially when dealing with props and hooks.
+//Generics is a major concept to be dealt with.
+
+
+//TS with hooks
+//When defining a hook, e.g useState, generics should be used to define what type of value does the state hold.
+
+const [string, setString] = useState<string>() //defaults to useState<null>() 
+
+//If it is an array of strings
+
+const [str,setStr] = useState<string[]>([])
+
+
+//Array of objects
+
+type ObjectType = {
+  name:string,
+  age:number
+}
+
+const [state,setState] = useState<ObjectType>({name:'',age:10})
 
