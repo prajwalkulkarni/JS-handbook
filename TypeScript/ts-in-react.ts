2fe17a72c44202e125c5dae33bcf7fa452a761d3
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-10-30 21:31:24 +0530

    Update ts-in-react.ts

--- TypeScript/ts-in-react.ts
+++ TypeScript/ts-in-react.ts
@@ -21,3 +21,35 @@ type ObjectType = {
 
 const [state,setState] = useState<ObjectType>({name:'',age:10})
 
+
+
+//If it's being used with useRef, it should be specified what exactly is the ref pointing to...
+
+const ref = useRef<HTMLInputElement>(null);
+
+//Writing any functional component in tsx require the component to be typed as React.FC
+
+const Component:React.FC = (props)=>{
+  return(...)
+         
+}
+
+//React.FC accepts generic parameters under a single object, that specifies all the prop items and their specified type
+type ObjectType = {
+  name:string,
+  age:number
+}
+const Component: React.FC<{items:ObjectType[],handler:Function,handlerWithParams: (args)=>void}> = (props) =>{
+  return(
+    <h1>{props.items.map(item=>return(<p>{item.name}</p>))}</h1>
+    )
+}
+
+const ParentComponent = ()=>{
+  
+  return(
+    <div>
+    <Component items={[{name:'Ram',age:18},{name:'Shyam',age:23}]} handler={()=>{console.log("This is a function")}} handlerWithParams = {(arg)=>console.log(`this is ${arg}`)}/>
+    </div>
+  )
+}
