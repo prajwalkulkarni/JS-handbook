9a26ccd698f13499ca6ad18cd0d08d807c1adb76
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-08 20:19:29 +0530

    Update nodejs.js

--- nodejs/nodejs.js
+++ nodejs/nodejs.js
@@ -1,1 +1,24 @@
+//NodeJS
 
+const http = express('http')
+
+http.createServer((req,res)=>{
+  res.end('<h1>Hello world!')
+})
+
+http.listen(PORT_NO);
+
+
+//NodeJS + express
+
+const express = require('express')
+
+const app = express()
+
+app.use((req,res,next)=>{
+  
+  res.send('<h1>Hello, world!</h1>')
+  next() //next() method to forward the request to other middleware
+})
+
+app.listen(PORT_NO);
