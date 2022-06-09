a932383c71c747cd51fb491a2e4cc327d0034a1f
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-10 00:00:29 +0530

    Update webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpackconfig = require('path/to/webpack.config.js')
 
 module.exports = merge(webpackconfig,{
@@ -32,7 +32,8 @@ module.exports = merge(webpackconfig,{
     }),
     new MiniCssExtractPlugin({
       filename:'[name].[contenthash].css'
-    })
+    }),
+    new CleanWebpackPlugin()
     ]
 })
   
