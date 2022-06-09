4e7aa0d6d6dff35962a712a43cfb9a8fcf73bb3f
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-09 22:39:01 +0530

    Create webpack.dev.js

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,28 @@
+const merge = require('webapck-merge')
+const path = require('path')
+const webapckConfig = require('paht/to/webpack.config.js')
+
+module.exports = merge(webpackConfig,{
+  mode:'development',
+  output: {
+    //path: path.resolve(__dirname,'dist'), //Absolute path, dist is a non existing directory that'll be created when the file is bundled
+    path:path.join(__dirname,'dist') ---> No need of using publicPath
+    filename:'bundle.js', //Anyname, suggested to use filename.[contentHash].js for cache busting, the content hash changes whenever the file changes.
+    //if not, a cached version will be served.
+    
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        use:[
+          'babel-loader',
+          ]
+      }
+    ]
+  },
+  
+})
+
+
+ 
