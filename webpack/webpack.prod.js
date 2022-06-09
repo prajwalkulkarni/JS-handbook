aebca30253aa410a1f382b93b6a59686ebb3802a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-09 23:36:09 +0530

    Update webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const webpackconfig = require('path/to/webpack.config.js')
 
@@ -18,11 +19,19 @@ module.exports = merge(webpackconfig,{
         'ts-loader'
        ],
       exclude:/node_modules/
-    }
+    },{
+      test:/\.scss$/,
+      use:[
+        'MiniCssExtractpLugin.loader, // Generate separate css file instead of converting css into commonjs module and injecting it to js file(Do it only in production, because it takes time to spit out css file).
+        'css-loader',
+        'sass-loader'
    ],
   plugins:[
     new HtmlWebpackPlugin({
       template:'paht/to/html'
+    }),
+    new MiniCssExtractPlugin({
+      filename:'[name].[contenthash].css'
     })
     ]
 })
