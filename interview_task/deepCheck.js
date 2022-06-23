3b44d1c0b599a3747579c9b9d8854f90e3327563
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-23 22:00:48 +0530

    Create deepCheck.js

---
+++ interview_task/deepCheck.js
@@ -0,0 +1,19 @@
+function deepCheck(val1,val2){
+    
+    if(typeof val1 !== typeof val2) return false;
+    
+    const primitives = ['number','undefined','string','boolean',null]
+    if(primitives.includes(typeof val1)){
+        return val1===val2
+    }
+    
+    if(Array.isArray(val1)){
+        if(val1.length !== val2.length) return false
+        
+        return val1.every((item,idx)=>deepCheck(item,val2[idx]))?true:false
+    }
+    
+   return JSON.stringify(val1) === JSON.stringify(val2) 
+}
+
+console.log(deepCheck({},[]))
