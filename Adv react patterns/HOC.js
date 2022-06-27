a467fc66290d72c1c5579e897cacda27907e8486
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-27 12:24:13 +0530

    Update HOC.js

--- Adv react patterns/HOC.js
+++ Adv react patterns/HOC.js
@@ -3,3 +3,26 @@
 //that wants to access this feature can wrap iteself to this shared component. HOCs do not mutate the original component, it instead adds some shared behavior the 
 //component and returns a new component.
 
+//Use cases
+// Protect private routes from unauthenticated users.
+
+import { useContext } from "react";
+import { Context } from "./store/context";
+
+const withWrapper = (Component) => {
+  return function (props) {
+    const ctx = useContext(Context);
+
+    if (ctx.isSignedIn) {
+      return <Component {...props} />;
+    } else {
+      return <h1>Sign in to view this message</h1>;
+    }
+  };
+};
+
+export default withWrapper;
+//The state can either be consumed from a single source of truth or can be consued via props.
+
+
+const HOCWrappedSecret = withWrapper(Secret);
