cfba54ba243cfe8e4ecf49ea19661a193255dce3
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-06-28 23:24:14 +0530

    Update render props.js

--- Adv react patterns/render props.js
+++ Adv react patterns/render props.js
@@ -35,3 +35,25 @@ export default function RenderProps(props) {
     </div>
   );
 }
+
+
+//---------------
+
+import React from "react";
+import { render } from "react-dom";
+
+export default class ContentComponent extends React.Component{    
+
+    render(){
+      return(
+        <div>
+          <h2>Today's date is {new Date().getDate()}</h2>
+
+          <article>
+            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+            </p>
+          </article>
+        </div>
+      )
+    }
+}
