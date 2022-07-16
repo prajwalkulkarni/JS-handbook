7c09124fc8949d467e96ec2ea0a63dd7d1d7f94b
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-16 23:24:34 +0530

    Update HOC.js

--- Adv react patterns/HOC.js
+++ Adv react patterns/HOC.js
@@ -1,4 +1,4 @@
-//HOC is similar to higher order function on component basis. HOCs are designed to create a ground for components that share a common bheavior. Instead of rewriting 
+//HOC is similar to higher order function on component basis. HOCs are designed to create a ground for components that share a common behavior. Instead of rewriting 
 //the same logic in every component that requires this functionality, HOCs can be written and this common set of functionalities can be placed there. This way any component
 //that wants to access this feature can wrap iteself to this shared component. HOCs do not mutate the original component, it instead adds some shared behavior the 
 //component and returns a new component.
