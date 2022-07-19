1a8b1e02045d8320bb4030f0a209916088d4083e
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2022-07-19 22:55:01 +0530

    Update useCustomHook.js

--- Adv react patterns/useCustomHook.js
+++ Adv react patterns/useCustomHook.js
@@ -1,10 +1,9 @@
 //Custom hooks are functions that follow the rule of hook,
 //Custom hooks alike regular hooks can be used either in other hooks or used in functional based component.
-//Like regular functions it can take arguments and return results (It is recommended to return results either in the form of an array or object to mimic the actual hook behavior
+//Like regular functions it can take arguments and return results (It is recommended to return results either in the form of an array or object to mimic the actual hook behavior)
 
 
 export default function useCustomHook(arg){
-  
   console.log(arg)
   
   return {
